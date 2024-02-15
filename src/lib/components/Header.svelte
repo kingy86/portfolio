10841bfa5359728fe0368e33be71d05141554748
Mark <nirvanafoo86@hotmail.com>
2024-02-15 21:55:51 +0000

    added flex classes to navbar

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,7 +5,7 @@
 <style>
     /* your styles go here */
 </style>
-<header class="w-full flex flex-row justify-between p-3">
+<header class="w-full flex flex-row p-3">
 <div class="logo">
   <img src="/images/logo.webp" alt="Logo" />
 </div>
--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -8,9 +8,9 @@
 
 <div class="drawer drawer-end">
   <input id="responsive-menu" type="checkbox" class="drawer-toggle" /> 
-    <div class="drawer-content flex flex-col">
+    <div class="drawer-content flex flex-col content-end">
       <!-- Navbar -->
-      <div class="w-full navbar">
+      <div class="w-1/2 navbar self-end justify-end">
         <div class="flex-none lg:hidden">
           <label for="responsive-menu" aria-label="open sidebar" class="btn btn-square btn-ghost text-white">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
