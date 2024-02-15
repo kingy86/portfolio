77af47a4a51d46de133b9101c3e688f582a7158e
Mark <nirvanafoo86@hotmail.com>
2024-02-15 22:00:28 +0000

    added padding to header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,7 +5,7 @@
 <style>
     /* your styles go here */
 </style>
-<header class="w-full flex flex-row p-3">
+<header class="w-full flex flex-row py-3 px-4 sm:px-16">
 <div class="logo">
   <img src="/images/logo.webp" alt="Logo" />
 </div>
