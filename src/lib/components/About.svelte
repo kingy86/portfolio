46f0d23863d00ccc997e1785de0cf752ebbb0ab9
Mark <nirvanafoo86@hotmail.com>
2024-03-09 16:51:53 +0000

    re-arranged about content

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -4,21 +4,21 @@
 
 <!-- markup (zero or more items) goes here -->
 <section id="about" class="flex justify-center">
-	<div class="w-10/12 py-16">
-		<h2 class="text-5xl font-semibold font-poppins leading-tight">
+	<div class="w-10/12 py-28 flex">
+		<h2 class="text-4xl font-semibold font-poppins leading-tight">
 			Providing cutting-edge digital solutions to startups and SMEs, to deliver effective results.
 		</h2>
-		<p class="text-base py-6 font-poppins">
+		<p class="text-base py-2 font-poppins">
 			A results-driven frontend web developer based in West Lothian, Scotland. I specialise in
 			providing cutting-edge digital solutions to startups and SMEs, leveraging my vast experience
-			to deliver effective results for clients. Throughout my career, I have had the opportunity to
-			work with businesses from various industries and have gained a deep understanding of their
-			unique needs and challenges. I pride myself on my ability to listen to my clients and
-			collaborate with them to develop customized solutions that drive success. By combining my
-			technical expertise with my strong communication skills, I have been able to build strong
-			relationships with clients and deliver solutions that exceed their expectations. If you're
-			looking for a reliable and experienced web developer who can help take your business to the
-			next level, I'm here to help.
+			to deliver effective results for clients.<br /><br /> Throughout my career, I have had the
+			opportunity to work with businesses from various industries and have gained a deep
+			understanding of their unique needs and challenges. I pride myself on my ability to listen to
+			my clients and collaborate with them to develop customized solutions that drive success.
+			<br /><br />By combining my technical expertise with my strong communication skills, I have
+			been able to build strong relationships with clients and deliver solutions that exceed their
+			expectations. If you're looking for a reliable and experienced web developer who can help take
+			your business to the next level, I'm here to help.
 		</p>
 	</div>
 </section>
